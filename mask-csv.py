6ea82a03ea5886604c01b26a8fbdefe8450d555e
zokikoz <77430357+zokikoz@users.noreply.github.com>
2021-06-13 14:05:25 +0300

    Writing file

--- mask-csv.py
+++ mask-csv.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Add mask to address row
 
 import sys
 import csv
@@ -7,6 +8,11 @@ results = []
 with open(sys.argv[1], mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-        row['address']+= sys.argv[2]
+        row['address'] += sys.argv[2]
         results.append(row)
-print(results)
+new_file = sys.argv[1] + '.mod'
+with open(new_file, 'w') as csv_file:
+    csv_writer = csv.DictWriter(csv_file, results[0].keys())
+    csv_writer.writeheader()
+    for row in results:
+        csv_writer.writerow(row)
